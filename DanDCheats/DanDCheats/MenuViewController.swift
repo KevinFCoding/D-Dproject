95e7cb8581324b192536ce0ae914764b53047ae3
TangUrb1 <urbaintanguy@gmail.com>
2021-01-08 04:36:32 +0000

    Creation of the Homepage storyboard and creation + navigation to Menu storyboard

---
+++ DanDCheats/DanDCheats/MenuViewController.swift
@@ -0,0 +1,19 @@
+//
+//  MenuViewController.swift
+//  DanDCheats
+//
+//  Created by user189012 on 08/01/2021.
+//
+
+import UIKit
+
+class MenuViewController: UIViewController {
+    
+    @IBOutlet var myScrollView: UIScrollView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+}
