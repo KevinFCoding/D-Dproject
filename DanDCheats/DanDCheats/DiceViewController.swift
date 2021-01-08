3b4eb90ce251d4cbf84c36c855a817a667596919
A <adrienamare@gmail.com>
2021-01-08 01:23:36 +0100

    j ai

--- DanDCheats/DanDCheats/DiceViewController.swift
+++ DanDCheats/DanDCheats/DiceViewController.swift
@@ -9,21 +9,28 @@ import UIKit
 
 class DiceViewController: UIViewController {
 
+    @IBOutlet var text: UILabel!
+    @IBOutlet var textfield: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        text.text = ""
+       
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+   
+    @IBAction func changetext(_ sender: Any) {
+        let toint = textfield.text!
+        let valeurMax:Int? = Int(toint)
+        if (valeurMax == nil) {
+            text.text = "Entre un nombre"
+        }
+        else{
+            if(valeurMax!<0){
+                text.text = "Trop petit"
+            }
+            else{
+                var number = Int.random(in: 0..<valeurMax!)
+                text.text = String(number)
+            }
+        }
     }
-    */
-
 }
